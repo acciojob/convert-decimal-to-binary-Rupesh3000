1317a7feea6c143ae773686a0cc5d75a703f4081
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2026-02-05 21:02:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function decimalToBinary(num) {
-  // your code here
+  if (num <= 0) return 0;
+  // your code here]
+
+  let ans = [];
+  while (num > 0) {
+    let bit = Math.floor(num % 2);
+    ans.push(bit);
+    num = Math.floor(num / 2);
+  }
+  return ans.reverse().join("");
 }
 const num = parseInt(prompt("Enter a number."));
 alert(decimalToBinary(num));
